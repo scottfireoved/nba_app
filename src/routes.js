a6a8d9e4e11a58960cffdecd3031af5b3b1fa247
scottfireoved <scottfireoved@gmail.com>
2019-10-22 22:20:28 -0700

    created videos route for showing videos.

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home/home';
 import News from './components/News/news';
+import Videos from './components/Videos/videos';
 import Layout from './hoc/Layout/layout';
 
 import NewsArticle from './components/Articles/News/Post/index';
@@ -15,6 +16,7 @@ class Routes extends Component {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/news" exact component={News} />
+          <Route path="/videos" exact component={Videos} />
           <Route path="/articles/:id" exact component={NewsArticle} />
           <Route path="/videos/:id" exact component={VideoArticle} />
         </Switch>
